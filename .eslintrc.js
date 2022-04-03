0e1a68fe4cb748bb1da63eb3cbaee139c28d8f52
varayac <victor.d.araya@gmail.com>
2022-04-03 17:13:56 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'warn',
+		'import/no-absolute-path': 'warn',
+	},
+};
